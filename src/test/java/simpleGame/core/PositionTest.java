9215d0f8db3231bbc9963b5db09467797f8e5cf9
besher AL ADDAM <beshoux@outlook.fr>
2023-11-15 12:18:44 +0100

    Ajout ficher Position test, avec les méthodes à tester

---
+++ src/test/java/simpleGame/core/PositionTest.java
@@ -0,0 +1,29 @@
+package simpleGame.core;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PositionTest {
+
+    @Test
+    @DisplayName("create object with same coordinates")
+    void copy() {
+    }
+
+    @Test
+    @DisplayName("create object next to coordinates")
+    void getPositionNextTo() {
+    }
+
+    @Test
+    @DisplayName("Is coords next to other coords")
+    void isNextTo() {
+    }
+
+    @Test
+    @DisplayName("coords equals to other")
+    void testEquals() {
+    }
+}
