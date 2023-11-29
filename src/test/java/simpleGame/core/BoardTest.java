8bd62dd8bd1786e7974c2b52654daca5ffed2039
besher AL ADDAM <beshoux@outlook.fr>
2023-11-29 12:01:45 +0100

    Board: test Constructeur dimensions du plateau

--- src/test/java/simpleGame/core/BoardTest.java
+++ src/test/java/simpleGame/core/BoardTest.java
@@ -1,5 +1,6 @@
 package simpleGame.core;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,11 +8,16 @@ import static org.junit.jupiter.api.Assertions.*;
 class BoardTest {
 
     @Test
+    @DisplayName("Test Constructeur")
     void testBoardConstruct() {
         Board board = new Board(2,4,4,3,4);
 
         /* Test position case bonus */
         assertEquals(board.getBonusSquare(), new Position(3,4));
+
+        /* Test dimensions du plateau */
+        assertEquals(board.getXSize(), 4);
+        assertEquals(board.getYSize(), 4);
     }
 
     @Test
